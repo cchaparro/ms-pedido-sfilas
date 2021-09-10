c529236d1fc966893f1fae45549efca4366c8afd
Cristian Chaparro <chaparro.cuadros@gmail.com>
2021-09-10 11:33:25 -0500

    Micro de prueba del sistema de filas

---
+++ src/main/java/com/cchaparro/ms/sFilas/MsSFilasApplication.java
@@ -0,0 +1,13 @@
+package com.cchaparro.ms.sFilas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSFilasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSFilasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cchaparro/ms/sFilas/MsSFilasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cchaparro.ms.sFilas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsSFilasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
